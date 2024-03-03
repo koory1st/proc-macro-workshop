0d1db5144c223bbc0ea3aef951e89ebb1201f136
koory1st <32436334@qq.com>
2024-03-03 14:05:50 +0800

    fix: add syn

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,11 @@
 use proc_macro::TokenStream;
+use syn;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
+    let st = syn::parse_macro_input!(input as syn::DeriveInput);
+
     let _ = input;
 
-    unimplemented!()
+    TokenStream::new()
 }
