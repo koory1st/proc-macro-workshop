8834380b88bc9b8db9a8de71462c6696f8b94d40
koory1st <32436334@qq.com>
2024-03-04 21:01:44 +0800

    fix: add builder struct

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,11 +1,20 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let st = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    let _ = input;
+    let ident = st.ident;
+    let data = st.data;
 
-    TokenStream::new()
+    let builder_name = format!("{}Builder", ident);
+    let builder_ident = syn::Ident::new(&builder_name, ident.span());
+
+    let rt = quote!(
+      struct #builder_ident{}
+    );
+
+    rt.into()
 }
